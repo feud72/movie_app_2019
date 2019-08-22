78dd19f772c89cc8de3f63672ceea47efc0a5af3
feud72 <feud72@gmail.com>
2019-08-22 13:01:57 +0900

    Updates

---
+++ precache-manifest.d9a939c67df8780dd210164e43579a8e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "33580a003ee72cb568b2bd575eb722be",
+    "url": "/movie_app_2019/index.html"
+  },
+  {
+    "revision": "c6fa22771052bfca5b96",
+    "url": "/movie_app_2019/static/css/main.f1c8a3a0.chunk.css"
+  },
+  {
+    "revision": "3350fcf69a8fdf7d4a1c",
+    "url": "/movie_app_2019/static/js/2.1c4cb735.chunk.js"
+  },
+  {
+    "revision": "c6fa22771052bfca5b96",
+    "url": "/movie_app_2019/static/js/main.1b4e048b.chunk.js"
+  },
+  {
+    "revision": "8e34f0f3f9415f236696",
+    "url": "/movie_app_2019/static/js/runtime~main.97c70ab9.js"
+  }
+]);
